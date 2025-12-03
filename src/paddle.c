e6171ede13054257426846b8a046e2e9b5e7afec
DANILO308 <dani2007futbolista@gmail.com>
2025-12-03 13:03:45 -0600

    funciones definidas

--- src/paddle.c
+++ src/paddle.c
@@ -1,12 +1,18 @@
 #include "paddle.h"
+#include "input.h"
+#include "datos.h"
 
 static Paddle paddle;
 
 void paddle_init() {
+    int paddle_y = paddle.x;
+    int paddle_x = WIDTH/2;
     // TODO: colocar el paddle centrado
 }
 
 void paddle_update(int direction) {
+    int move;
+    move = input_update();
     // TODO: mover el paddle según la dirección (-1 izquierda, 1 derecha)
 }
 
